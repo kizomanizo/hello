eb8665a6e1f9c67ef58976bac5d71f904ac57123
Bwana Kizito <kizomanizo@gmail.com>
2018-10-12 09:42:51 +0300

    Compillable API

---
+++ src/test/java/com/hello/UserTest.java
@@ -0,0 +1,32 @@
+package com.hello;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UserTest {
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void setName() {
+    }
+
+    @Test
+    public void getEmail() {
+    }
+
+    @Test
+    public void setEmail() {
+    }
+}
